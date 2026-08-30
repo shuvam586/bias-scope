f158b1f28d75592d33d7b4341c0a3e6423a892c9
Shuvam Mitra <shuvammitra05@gmail.com>
2026-08-31 00:05:47 +0530

    added more sources, some from google news rss

--- backend/app/ingestion/sources.py
+++ backend/app/ingestion/sources.py
@@ -1,73 +1,139 @@
 SOURCES = {
-    "ndtv" : {
+    "ndtv": {
         "name": "NDTV",
         "feeds": [
             "https://feeds.feedburner.com/ndtvnews-top-stories",
-            "https://feeds.feedburner.com/ndtvnews-trending-news"
-        ]
+            "https://feeds.feedburner.com/ndtvnews-trending-news",
+        ],
     },
-
     "indian_express": {
         "name": "The Indian Express",
         "feeds": [
             "https://indianexpress.com/section/india/feed/",
-            "https://indianexpress.com/section/trending/trending-in-india/feed/"
-        ]
+            "https://indianexpress.com/section/trending/trending-in-india/feed/",
+        ],
     },
-
     "hindustan_times": {
         "name": "Hindustan Times",
         "feeds": [
             "https://www.hindustantimes.com/feeds/rss/india-news/rssfeed.xml",
-            "https://www.hindustantimes.com/feeds/rss/trending/rssfeed.xml"
-        ]
+            "https://www.hindustantimes.com/feeds/rss/trending/rssfeed.xml",
+        ],
     },
-
     "india_today": {
         "name": "India Today",
         "feeds": [
             "https://www.indiatoday.in/rss/home",
             "https://www.indiatoday.in/rss/1206614",
-            "https://www.indiatoday.in/rss/1206584"
-        ]
+            "https://www.indiatoday.in/rss/1206584",
+        ],
     },
-
     "hindu": {
         "name": "The Hindu",
         "feeds": [
             "https://www.thehindu.com/news/cities/feeder/default.rss",
             "https://www.thehindu.com/news/states/feeder/default.rss",
             "https://www.thehindu.com/news/national/feeder/default.rss",
-            "https://www.thehindu.com/news/international/feeder/default.rss"
-        ]
+            "https://www.thehindu.com/news/international/feeder/default.rss",
+        ],
     },
-
     "toi": {
         "name": "The Times of India",
         "feeds": [
             "http://timesofindia.indiatimes.com/rssfeedstopstories.cms",
             "http://timesofindia.indiatimes.com/rssfeeds/-2128936835.cms",
             "http://timesofindia.indiatimes.com/rssfeeds/296589292.cms",
-            "http://timesofindia.indiatimes.com/rssfeeds/1898055.cms"
-        ]
+            "http://timesofindia.indiatimes.com/rssfeeds/1898055.cms",
+        ],
     },
-
     "news18": {
         "name": "News18",
         "feeds": [
-            "https://www.news18.com/commonfeeds/v1/eng/rss/viral.xml",
             "https://www.news18.com/commonfeeds/v1/eng/rss/india.xml",
-            "https://www.news18.com/commonfeeds/v1/eng/rss/world.xml"
-        ]
+            "https://www.news18.com/commonfeeds/v1/eng/rss/world.xml",
+        ],
     },
-
     "firstpost": {
         "name": "FirstPost",
         "feeds": [
             "https://www.firstpost.com/commonfeeds/v1/mfp/rss/india.xml",
             "https://www.firstpost.com/commonfeeds/v1/mfp/rss/world.xml",
             "https://www.firstpost.com/commonfeeds/v1/mfp/rss/entertainment.xml",
-            "https://www.firstpost.com/commonfeeds/v1/mfp/rss/health.xml"
-        ]
-    }
+            "https://www.firstpost.com/commonfeeds/v1/mfp/rss/health.xml",
+        ],
+    },
+    "telegraph": {
+        "name": "The Telegraph India",
+        "feeds": [
+            "https://news.google.com/rss/search?q=site%3Atelegraphindia.com&hl=en-IN&gl=IN&ceid=IN:en"
+        ],
+    },
+    "new_indian_expr": {
+        "name": "The New Indian Express",
+        "feeds": [
+            "https://news.google.com/rss/search?q=site:newindianexpress.com&hl=en-IN&gl=IN&ceid=IN:en"
+        ],
+    },
+    "deccan_herald": {
+        "name": "Deccan Herald",
+        "feeds": [
+            "https://news.google.com/rss/search?q=site:deccanherald.com&hl=en-IN&gl=IN&ceid=IN:en"
+        ],
+    },
+    "statesman": {
+        "name": "Statesman",
+        "feeds": [
+            "https://news.google.com/rss/search?q=site:thestatesman.com&hl=en-IN&gl=IN&ceid=IN:en"
+        ],
+    },
+    "tribune": {
+        "name": "The Tribune",
+        "feeds": [
+            "https://publish.tribuneindia.com/newscategory/india/feed/",
+            "https://publish.tribuneindia.com/newscategory/world/feed/",
+            "https://publish.tribuneindia.com/newscategory/top-headlines/feed/",
+        ],
+    },
+    "pioneer": {
+        "name": "The Pioneer",
+        "feeds": [
+            "https://news.google.com/rss/search?q=site:dailypioneer.com&hl=en-IN&gl=IN&ceid=IN:en"
+        ],
+    },
+    "deccan_chronicle": {
+        "name": "Deccan Chronicle",
+        "feeds": [
+            "https://news.google.com/rss/search?q=site:deccanchronicle.com&hl=en-IN&gl=IN&ceid=IN:en"
+        ],
+    },
+    "freepressjournal": {
+        "name": "Free Press Journal",
+        "feeds": [
+            "https://news.google.com/rss/search?q=site:freepressjournal.in&hl=en-IN&gl=IN&ceid=IN:en"
+        ],
+    },
+    "wire": {
+        "name": "The Wire",
+        "feeds": [
+            "https://news.google.com/rss/search?q=site:thewire.in&hl=en-IN&gl=IN&ceid=IN:en"
+        ],
+    },
+    "scroll_in": {
+        "name": "Scroll.in",
+        "feeds": [
+            "https://news.google.com/rss/search?q=site:scroll.in&hl=en-IN&gl=IN&ceid=IN:en"
+        ],
+    },
+    "print": {
+        "name": "The Print",
+        "feeds": [
+            "https://news.google.com/rss/search?q=site:theprint.in&hl=en-IN&gl=IN&ceid=IN:en"
+        ],
+    },
+    "quint": {
+        "name": "The Quint",
+        "feeds": [
+            "https://news.google.com/rss/search?q=site:thequint.com&hl=en-IN&gl=IN&ceid=IN:en"
+        ],
+    },
 }
